3ea3032226d1142831c1b6662d63e79b81955085
GiladPachter <gilad.pachter@gmail.com>
2026-03-14 12:08:50 +0200

    LogSmith - v1.9.1 - logsmith-1.9.1-py3-none-any.whl

--- LogSmith/__init__.py
+++ LogSmith/__init__.py
@@ -16,7 +16,7 @@ try:
     from importlib.metadata import version
     __version__ = version("LogSmith")
 except Exception:
-    __version__ = "1.9.0"
+    __version__ = "1.9.1"
 
 
 # LogSmith's calling card
