bf8490e9e47de12c2dfb32d60d41c4430ef840d4
GiladPachter <gilad.pachter@gmail.com>
2026-03-17 01:42:44 +0200

    LogSmith - v1.9.3 - multiple fixes for logs formatting - coverage: 72.66%

--- LogSmith/__init__.py
+++ LogSmith/__init__.py
@@ -16,7 +16,7 @@ try:
     from importlib.metadata import version
     __version__ = version("LogSmith")
 except Exception:
-    __version__ = "1.9.2"
+    __version__ = "1.9.3"
 
 
 # LogSmith's calling card
