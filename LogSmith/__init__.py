61a9f7ce33346927bba6e881c7450f33739f9291
GiladPachter <gilad.pachter@gmail.com>
2026-03-20 08:18:44 +0200

    LogSmith - v1.9.4 - AsyncSmartLogger:   -- named parameters formatting fix   -- auditing rotation_logic attached   -- audited logger name, not auditing logger name - coverage: 78.04%

--- LogSmith/__init__.py
+++ LogSmith/__init__.py
@@ -16,7 +16,7 @@ try:
     from importlib.metadata import version
     __version__ = version("LogSmith")
 except Exception:
-    __version__ = "1.9.3"
+    __version__ = "1.9.4"
 
 
 # LogSmith's calling card
