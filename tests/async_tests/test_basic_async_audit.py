ca32af59a1dfdec95efb261023199009002c36c7
GiladPachter <gilad.pachter@gmail.com>
2026-04-01 01:25:31 +0300

    LogSmith - v1.9.5 - fixed async RotateFirst - auditing NDJSON is now possible # - coverage:  91.89%

--- tests/async_tests/test_basic_async_audit.py
+++ tests/async_tests/test_basic_async_audit.py
@@ -3,29 +3,26 @@ import logging
 import pytest
 
 from LogSmith.async_smartlogger import AsyncSmartLogger
-from LogSmith.formatter import OutputMode
+from LogSmith.formatter import OutputMode, LogRecordDetails
 
 
 @pytest.mark.asyncio
 async def test_audit_logger_basic(tmp_path):
-    # main logger
     main = AsyncSmartLogger("main_audit", logging.INFO)
     main.add_file(str(tmp_path), "main.log", output_mode=OutputMode.PLAIN)
 
-    # audit logger
-    audit = AsyncSmartLogger("audit_sink", logging.INFO)
-    audit.add_file(str(tmp_path), "audit.log", output_mode=OutputMode.JSON)
-
-    # wire audit
-    AsyncSmartLogger._AsyncSmartLogger__audit_enabled = True
-    AsyncSmartLogger._AsyncSmartLogger__audit_logger = audit
+    await AsyncSmartLogger.audit_everything(
+        log_dir=str(tmp_path),
+        logfile_name="audit.log",
+        rotation_logic=None,
+        details=LogRecordDetails(),
+        # NDJSON_output=False (or True, depending on what you want)
+    )
 
     await main.a_info("hello-audit")
-    # await main.__queue.join()
-    # await audit.__queue.join()
-    await main._AsyncSmartLogger__queue.join()  # accessing private member. do not use outside of test suite
-    await audit._AsyncSmartLogger__queue.join() # accessing private member. do not use outside of test suite
+    await main._AsyncSmartLogger__queue.join()
+    await AsyncSmartLogger._AsyncSmartLogger__audit_logger._AsyncSmartLogger__queue.join()
 
     text = (tmp_path / "audit.log").read_text(encoding="utf-8").strip()
     assert "hello-audit" in text
-    assert "main_audit" in text  # prefixed original logger name
+    assert "main_audit" in text
